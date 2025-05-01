57e1835dd7b97055dd7bbc605718259ffd9835cb
nafiuibnemazhar <nafiuibnemazhar0@gmail.com>
2025-05-01 23:45:46 +0600

    Simple login validation added & home page added

--- js/login.js
+++ js/login.js
@@ -4,5 +4,20 @@ document.getElementById("login-btn").addEventListener("click", function (e) {
   e.preventDefault();
 
   const phoneNumber = document.getElementById("phone-number").value;
-  console.log(phoneNumber);
+  const pinNumber = document.getElementById("pin-number").value;
+
+  //   console.log(phoneNumber);
+  //   console.log(pinNumber);
+
+  if (phoneNumber === "" || pinNumber === "") {
+    alert("Please fill in all fields");
+    return;
+  }
+
+  if (phoneNumber === "01600201234" && pinNumber === "1234") {
+    alert("Login successfully done!");
+    window.location.href = "home.html";
+  } else {
+    alert("Login failed! Please check you phone number and pin");
+  }
 });
