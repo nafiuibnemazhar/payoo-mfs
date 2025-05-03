e2e642e6d1ae23e5af164022b3de3dd4c0fe59de
nafiuibnemazhar <nafiuibnemazhar0@gmail.com>
2025-05-03 20:31:33 +0600

    add money calculation added

--- js/home.js
+++ js/home.js
@@ -7,4 +7,14 @@ document
 
     const addMoneyInput = document.getElementById("input-add-money").value;
     const inputPinNumber = document.getElementById("input-pin-number").value;
+
+    if (inputPinNumber === "1234") {
+      console.log("Adding money to account");
+
+      const balance = document.getElementById("account-balance").innerText;
+      const newBalance = parseFloat(balance) + parseFloat(addMoneyInput);
+      document.getElementById("account-balance").innerText = newBalance;
+    } else {
+      alert("Failed to add money to account. Please try afain!!!");
+    }
   });
