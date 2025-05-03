152ce02045e35e26ea6fb7369891e405d4a9ba30
nafiuibnemazhar <nafiuibnemazhar0@gmail.com>
2025-05-03 19:24:24 +0600

    added add money form and add money event handler

---
+++ js/home.js
@@ -0,0 +1,10 @@
+// console.log("home js added");
+
+document
+  .getElementById("btn-add-money")
+  .addEventListener("click", function name(e) {
+    e.preventDefault();
+
+    const addMoneyInput = document.getElementById("input-add-money").value;
+    const inputPinNumber = document.getElementById("input-pin-number").value;
+  });
